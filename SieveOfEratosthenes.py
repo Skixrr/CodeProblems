aef29d1d0427a06838fcc32372d9a0a4527c8a62
Skixrr <camrongeorgeshort@gmail.com>
2024-07-10 12:32:12 +0100

    Update SieveOfEratosthenes.py

--- SieveOfEratosthenes.py
+++ SieveOfEratosthenes.py
@@ -1,12 +1,11 @@
-num = 200
+num = 100
 primes = []
 
 #Sieve
 prime = [True for i in range(num+1)] #Boolean array of length num
 p = 2
 while (p**2 <= num):
-    # If prime[p] is not
-    # changed, then it is a prime
+    # If prime[p] is not changed, then it is a prime
     if (prime[p] == True): 
         # Updating all multiples of p
         for i in range(p**2, num+1, p):
