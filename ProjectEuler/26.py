4223e8e7f2ae175a94ff5311a69dce3b78cad68e
Skixrr <109033971+Skixrr@users.noreply.github.com>
2025-05-04 13:07:05 +0100

    Solved 26

--- ProjectEuler/26.py
+++ ProjectEuler/26.py
@@ -1,43 +1,49 @@
-num = 1000
-primes = []
-d = []
+def Sieve(num):
+    primes = []
+    #
+    #Sieve
+    prime = [True for i in range(num+1)] #Boolean array of length num
+    p = 2
+    while (p**2 <= num):
+        # If prime[p] is not changed, then it is a prime
+        if (prime[p] == True): 
+            # Updating all multiples of p
+            for i in range(p**2, num+1, p):
+                prime[i] = False
+        p += 1
+    #
+    # Print all prime numbers
+    for p in range(2, num+1):
+        if prime[p]:
+            primes.append(p)
+    return primes
+primes = Sieve(1000)
 
-def PatternLen(index,offset):
-    global d
-    temp = ''
-    
-
-#Sieve
-prime = [True for i in range(num+1)] #Boolean array of length num
-p = 2
-while (p**2 <= num):
-    # If prime[p] is not changed, then it is a prime
-    if (prime[p] == True): 
-        # Updating all multiples of p
-        for i in range(p**2, num+1, p):
-            prime[i] = False
-    p += 1
- 
-# Add all prime numbers to array
-for p in range(2, num+1):
-    if prime[p]:
-        primes.append(p)
-        d.append(p)
-
-
-
-for i in range(0,len(d)):
-    d[i] = 1/d[i]
-    d[i] = str(d[i])
-    d[i] = d[i][2::]
-    if (int(d[i]) * primes[i]) % 10 == 0:
-        d[i] = False
+def division(N):
+    print(N)
+    count = 0
+    M = 1
+    if N > 100:
+        M *= 1000
+    elif N > 10:
+        M *= 100
     else:
-        d[i] = str(d[i])[:-1]
+        M *= 10
+    M = M%N
+    while M != 1 and M!=0:
+        if N > 100:
+            M *= 1000
+        elif N > 10:
+            M *= 100
+        else:
+            M *= 10
+        M = M%N
+        count += 1
+    return count
 
-for i in range(0,len(d)):
-    if type(d[i]) != str:
-        continue
-    else:
-        temp = ''
-        for i in d[i]:
+mx = [0,0]
+for i in primes:
+    C = division(i)
+    if mx[1] < C:
+        mx = [i,C]
+print(mx)
